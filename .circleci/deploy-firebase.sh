beb7c63f727dbad2d6437827ee911001fc3f7b12
Khien Pham <phamkhien2309@gmail.com>
2025-08-12 11:16:25 +0700

    Setup React project (#3)

---
+++ .circleci/deploy-firebase.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+# This script deploys the Firebase project based on the branch.
+npm install --no-save firebase-tools
+
+if [ "${CIRCLE_BRANCH}" = "dev" ]; then
+  echo "$DEV_GOOGLE_CREDENTIALS" > credentials.json
+  GOOGLE_APPLICATION_CREDENTIALS=credentials.json ./node_modules/.bin/firebase deploy --project tenant-fe
+fi
+if [ "${CIRCLE_BRANCH}" = "main" ]; then
+  echo "$DEV_GOOGLE_CREDENTIALS" > credentials.json
+  GOOGLE_APPLICATION_CREDENTIALS=credentials.json ./node_modules/.bin/firebase deploy --project tenant-fe
+fi
