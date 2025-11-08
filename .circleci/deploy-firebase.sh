1e273691c09214a18f1482dc3b8d309581ffd530
Khien <kcp@cct-technology.com>
2025-11-08 16:01:34 +0700

    .

--- .circleci/deploy-firebase.sh
+++ .circleci/deploy-firebase.sh
@@ -7,7 +7,13 @@ if [ "${CIRCLE_BRANCH}" = "dev" ]; then
   echo "$DEV_GOOGLE_CREDENTIALS" > credentials.json
   GOOGLE_APPLICATION_CREDENTIALS=credentials.json ./node_modules/.bin/firebase deploy --project tenant-fe
 fi
+
 if [ "${CIRCLE_BRANCH}" = "main" ]; then
   echo "$DEV_GOOGLE_CREDENTIALS" > credentials.json
   GOOGLE_APPLICATION_CREDENTIALS=credentials.json ./node_modules/.bin/firebase deploy --project tenant-fe
+fi
+
+if [ "${CIRCLE_BRANCH}" = "cct" ]; then
+  echo "$DEV_GOOGLE_CREDENTIALS" > credentials.json
+  GOOGLE_APPLICATION_CREDENTIALS=credentials.json ./node_modules/.bin/firebase deploy cct --project tenant-fe
 fi
